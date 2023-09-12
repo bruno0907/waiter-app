f890f6cf9866094a7306846bbe83e7b2b0a51389
Bruno Mariani <bruno0907@gmail.com>
2023-09-12 19:24:58 -0300

    feat: added mongoose

--- api/src/index.ts
+++ api/src/index.ts
@@ -1,14 +1,26 @@
-import express, { Request, Response } from 'express';
+import express, { Response } from 'express';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+dotenv.config();
+
+const databaseUrl = process.env.DATABASE_URL!;
 
 const app = express();
-
 app.use(express.json());
 
-app.get('/health', (req: Request, res: Response) => {
+app.get('/health', (_, res: Response) => {
   res.status(200).json({ status: 'healthy' });
   res.end();
 });
 
-app.listen(3000, () => {
-  console.log('🔥 Server is running on http://localhost:3000');
-});
+mongoose.connect(databaseUrl)
+  .then(() => {
+    console.log('🗳️ Conectado ao MongoDB!!!');
+
+    app.listen(3000, () => {
+      console.log('🔥 Server is running on http://localhost:3000!!!');
+    });
+  })
+  .catch(() => console.log('❌ Erro ao se conectar ao MongoDB!!!'));
+
+
