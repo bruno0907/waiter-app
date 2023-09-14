5fcf01a28c98043e86d569adadf48f7892900ce9
Bruno Mariani <bruno0907@gmail.com>
2023-09-14 16:46:55 -0300

    chore: added styled-components deps and project assets

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,8 +1,11 @@
+import { GlobalStyles } from "./styles/GlobalStyles"
+
 export function App() {
   return (
     <>
+      <GlobalStyles />
       <h1>Waiter App</h1>
     </>
-  )
-}
+    )
+  }
 
